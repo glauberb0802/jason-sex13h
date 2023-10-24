6b20d558d336a218ae9f5b54cbe210cb2d6f7b7a
glauberb0802 <111463416+glauberb0802@users.noreply.github.com>
2023-10-24 11:50:12 -0300

    Update script.lua

--- assets/preload/data/drowned-memoirs/script.lua
+++ assets/preload/data/drowned-memoirs/script.lua
@@ -7,3 +7,19 @@ function onCreate()
 	setPropertyFromClass('substates.GameOverSubstate', 'loopSoundName', 'gameOver'); --file goes inside music/ folder
 	setPropertyFromClass('substates.GameOverSubstate', 'endSoundName', 'gameOverEnd'); --file goes inside music/ folder
 end
+function onStartCountdown()
+	if not allowEnd and not seenCutscene then
+		startVideo('jason-cutscene-phase-1-intro');
+		allowEnd = true;
+		return Function_Stop;
+	end
+	return Function_Continue;
+end
+function onEndSong()
+	if not allowEnd and not seenCutscene then
+		startVideo('jason-cutscene-phase-1-end');
+		allowEnd = true;
+		return Function_Stop;
+	end
+	return Function_Continue;
+end
